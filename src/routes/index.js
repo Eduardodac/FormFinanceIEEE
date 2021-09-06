168eb8de93530f0286a3d191f70565d606cc7a73
Eduardodac <eduardo.dac@outlook.com>
2021-09-06 13:43:32 -0500

    prueba del color azul

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,6 @@ const indexController = require('../controllers/indexController');
 
 /* GET home page. */
 router.get('/', indexController.main);
-router.get('/mini', indexController.user);
+router.post('/', indexController.user);
 
 module.exports = router;
