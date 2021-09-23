25e092720bdfb4c36ec4fcd0808466b65345c5eb
Eduardodac <eduardo.dac@outlook.com>
2021-09-23 18:26:59 -0500

    borrando registros

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,6 +18,6 @@ let validateRegister = [
 /* GET home page. */
 router.get('/', indexController.main);
 router.post('/', validateRegister, indexController.register);
-router.get('/asd/rti/bd', indexController.lista)
+// router.get('/asd/rti/bd', indexController.lista)
 
 module.exports = router;
